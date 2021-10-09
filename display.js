8fd4478a5269df449648964b4948e8a6b4700c92
mprinceps <64213121+bcp2415@users.noreply.github.com>
2021-10-09 15:41:23 +0000

    adds export and import statements to make modules work with index.js

--- display.js
+++ display.js
@@ -35,3 +35,6 @@ function printTestScores(index,test,students,scores){
   return;
 }
 
+module.exports = {
+  printAll: printAll
+}
--- randomSelect.js
+++ randomSelect.js
@@ -1,5 +1,10 @@
 function randomFromArray(arr){
   //Your code here to select a random element from the array passed to the function.
+  let generateRandomElement = Math.floor(Math.random() * arr.length);
+  const selection = arr[generateRandomElement];
+
+  return selection;
 }
 
 //TODO: Export the randomFromArray function.
+module.exports = randomFromArray;
